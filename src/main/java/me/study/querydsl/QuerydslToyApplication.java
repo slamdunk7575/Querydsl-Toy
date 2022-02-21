55d045b5dc1396c7438f5e90940a96f32a702e14
slamdunk7575 <slamdunk7575@gmail.com>
2022-02-22 02:21:54 +0900

    (feat) init commit

---
+++ src/main/java/me/study/querydsl/QuerydslToyApplication.java
@@ -0,0 +1,13 @@
+package me.study.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslToyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/study/querydsl/QuerydslToyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
