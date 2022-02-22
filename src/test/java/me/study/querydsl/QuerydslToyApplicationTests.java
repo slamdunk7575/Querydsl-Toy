cce6c69ce6b475681c25fa4041a51f67a3d2165d
slamdunk7575 <slamdunk7575@gmail.com>
2022-02-22 22:31:02 +0900

    (feat) 테스용(Hello) 엔티티 추가 및 QueryDsl 테스트

--- src/test/java/me/study/querydsl/QuerydslToyApplicationTests.java
+++ src/test/java/me/study/querydsl/QuerydslToyApplicationTests.java
@@ -1,13 +1,37 @@
 package me.study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import me.study.querydsl.entity.Hello;
+import me.study.querydsl.entity.QHello;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class QuerydslToyApplicationTests {
+
+    @Autowired
+    // @PersistenceContext
+    private EntityManager em;
 
     @Test
     void contextLoads() {
-    }
+        Hello hello = new Hello();
+        em.persist(hello);
 
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = query.selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
 }
