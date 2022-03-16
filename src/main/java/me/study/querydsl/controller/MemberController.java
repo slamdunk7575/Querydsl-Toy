1c2658ee52343a590c13ec41d395890c093e40f5
slamdunk7575 <slamdunk7575@gmail.com>
2022-03-16 19:56:13 +0900

    (feat) 컨트롤러 추가

--- src/main/java/me/study/querydsl/controller/MemberController.java
+++ src/main/java/me/study/querydsl/controller/MemberController.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import me.study.querydsl.dto.MemberSearchCondition;
 import me.study.querydsl.dto.MemberTeamDto;
 import me.study.querydsl.repository.MemberJpaRepository;
+import me.study.querydsl.repository.MemberRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +17,20 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.search(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageComplex(condition, pageable);
     }
 }
